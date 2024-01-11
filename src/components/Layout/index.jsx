3bc900f89aa97f003d156f7f9b90c8feb5319448
codegod61 <traceb200@gmail.com>
2024-01-12 03:07:46 +0700

    menambahkan favicon.ico pada layout

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,12 +1,17 @@
 import Header from '@/components/Header_Footer/Header'
 import Footer from '../Header_Footer/Footer';
+import Head from 'next/head';
 
 export default function Layout(props) {
-    return (
-      <div>
-        <Header />
-        {props.children}
-        <Footer/>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <Head>
+        <link rel="icon" href="../../../public/favicon.ico" />
+      </Head>
+
+      <Header />
+      {props.children}
+      <Footer />
+    </div>
+  );
+}
